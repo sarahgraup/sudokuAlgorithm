fcb55d0617421d1799906f0eb9b9337b53bf2c60
sarah <sarahgraup@g.ucla.edu>
2024-01-09 14:20:13 -0800

    fixed assign value bug

--- sudoku.js
+++ sudoku.js
@@ -2,72 +2,76 @@
 const fsP = require('fs/promises');
 
 
-class Sudoku{
-    constructor(board){
-        this.empty = [];
+//subgrid fucks it up 
+
+
+class Sudoku {
+    constructor(board) {
+        this.empty = {};
         this.row = [];
         this.col = [];
         this.subgrid = [];
         this.board = board;
         this.boardSize = 9;
         this.subgridSize = 3;
+        this.posValues = {};
 
     }
 
     /**  */
-    static async getSudoku(file){
+    static async getSudoku(file) {
 
         let contents;
         try {
-           contents = await fsP.readFile(`${file}`, "utf8");
-         
+            contents = await fsP.readFile(`${file}`, "utf8");
+
         } catch (err) {
-          process.exit(1);
+            process.exit(1);
         }
-    
+
         const board = contents.split('\r\n').map(r => r.trim()).map(c => c.split(' ').map(Number));
-       
+
         return new Sudoku(board);
     }
 
     /**initilizes sudoku board with new board */
-    createBoard(){
+    createBoard() {
 
-  
 
-        for(let i=0; i<this.boardSize; i++){
+
+        for (let i = 0; i < this.boardSize; i++) {
             this.subgrid.push([]);
         }
 
-        for(let r = 0; r<this.boardSize; r++){
+        for (let r = 0; r < this.boardSize; r++) {
             this.row.push(this.board[r]);
             this.col.push([]);
-           
-            for(let c=0; c<this.boardSize; c++){
+
+            for (let c = 0; c < this.boardSize; c++) {
                 this.col[r].push(this.board[c][r]);
-                // const subgridRow = Math.floor(r / this.subgridSize);
-                // const subgridCol = Math.floor(c / this.subgridSize);
-                // const subgridIndex = subgridRow * this.subgridSize + subgridCol;
-                const subgridIndex = this.getSubgridIndex(r,c);
            
+                const subgridIndex = this.getSubgridIndex(r, c);
+
                 this.subgrid[subgridIndex].push(this.board[r][c]);
-         
+
 
             }
-         
+
         }
-    
-        // console.log(this.col);
-        // console.log("sub", this.subgrid);
-        // console.log("board", this.board);
-        
+
+  
+
     }
 
     /**returns subgrid index based on row and col */
-    getSubgridIndex(r,c){
+    getSubgridIndex(r, c) {
         const subgridRow = Math.floor(r / this.subgridSize);
+
+
         const subgridCol = Math.floor(c / this.subgridSize);
+
         const subgridIndex = subgridRow * this.subgridSize + subgridCol;
+
 
         return subgridIndex;
 
@@ -78,17 +82,18 @@ class Sudoku{
      * The main recursive function for solving the Sudoku. It stops if the Sudoku is solved, 
      * otherwise it selects an unassigned cell and tries possible values for that cell.
     */
-    cdclSolver(){
-        if(this.isSudokuSolved()){
+    cdclSolver() {
+        if (this.isSudokuSolved()) {
             return true;
         }
+
 
         let cell = this.selectUnassignedCell();
 
 
     }
 
-   
+
 
     /**chooses a cell to assign value based on heurisitics 
      * This function implements the MRV and Degree heuristics.
@@ -96,20 +101,108 @@ class Sudoku{
      * If there's a tie, it then uses the Degree heuristic to select the cell 
      * with the most constraints with other unassigned cells.
     */
-    selectUnassignedCell(){
+    selectUnassignedCell() {
+
+        let foundSingleElement = false;
+
         this.getUnassignedCells();
-        let numMinOptions = Infinity;
-        let maxDegree = -1;
-        let bestCell = null;
-        //change it so first it just checks if one value and then assign. then alters board, and does again
-        this.empty.forEach(([r,c]) => {
-            const validOptions = this.numPossibleValues(r, c);
-            if(validOptions.length===1){
-                this.assignValue(r, c, validOptions[0]);
+        console.log("first ple", this.empty);
+
+        //checks if all unassigned cells have more than 1 possible value
+
+
+
+        this.numPossibleValues();
+
+   
+
+        for(let rowKey of Object.keys(this.posValues)){
+            for(let colKey of Object.keys(this.posValues[rowKey])){
+                let values = this.posValues[rowKey][colKey];
+                if(values.length===1){
+                    foundSingleElement = true;
+       
+                    this.assignValue(rowKey, colKey, values[0]);
+
+                }
             }
+        }
+
+
+
+        if(foundSingleElement===false){
+            return;
+        }else{
+            this.selectUnassignedCell();
+        }
+
+        
+       
+        // if(unassignedCells.every(cell=> cell.length > 1)){
+        //     console.log("all unassigned cells have more than one pos val");
+        //     return;
+        // }
+
+
+
+        // let numMinOptions = Infinity;
+        // let maxDegree = -1;
+        // let bestCell = null;
+
+        // let onlyOneOption = true;
+
+        // while(onlyOneOption){
+
+
+
+        //     for(let r=0; r<this.empty.length; r++){
+        //         for(let c=0; c<this.empty[r].length; c++){
+        //             const validOptions = this.numPossibleValues(r, c);
+        //             if(validOptions.length===1){
+        //                 onlyOneOption = true;
+        //                 console.log("r", r, "c", c, "only one option", validOptions);
+        //                 this.assignValue(r, c, validOptions[0]);
+        //                 break;
+        //             }else{
+        //                 onlyOneOption = false;
+        //             }
+
+
+
+        //         }
+        //         if(onlyOneOption===true){
+        //             r--;
+        //         }
+        //     }
+        //     this.getUnassignedCells();
+        // }
+        // onlyOneOption = false;
+        // this.getUnassignedCells();
+        // this.empty.forEach(([r,c]) => {
+
+        //     const validOptions = this.numPossibleValues(r, c);
+        //     if(validOptions.length===1){
+        //         onlyOneOption = true;
+        //         this.assignValue(r, c, validOptions[0]);
+        //         console.log(this.board);
+        //     }
+
+        // });
+
+        // console.log("NEXT ONEEEEEEEEEEEEEEEEEEE");
+
+
+        //change it so first it just checks if one value and then assign. then alters board, and does again
         // this.empty.forEach(([r,c]) => {
         //     const validOptions = this.numPossibleValues(r, c);
-        //     if(validOptions.length>1){
+        //     if(validOptions.length===1){
+        //         this.assignValue(r, c, validOptions[0]);
+        //     }
+
+        // });
+        // this.getUnassignedCells();
+        // this.empty.forEach(([r,c]) => {
+        //     const validOptions = this.numPossibleValues(r, c);
         //         if(validOptions.length < numMinOptions){
         //             numMinOptions = validOptions.length;
         //             maxDegree = this.countConstraints(r, c);
@@ -121,29 +214,17 @@ class Sudoku{
         //                 maxDegree = degree;
         //                 bestCell = [r,c];
         //             }
-                    
-    
+
+
         //         }
 
-        //     }
-        //     else{
-        //         this.assignValue(r, c, validOptions[0]);
+        //     });
 
-        //     }
-            
-            // const degree = this.countConstraints(r, c);
 
-            // if(validOptions.length< numMinOptions || (validOptions.length===numMinOptions && degree> maxDegree) ){
-            //     numMinOptions = validOptions;
-            //     maxDegree = degree;
-            //     bestCell = [r,c];
-            // }
-
-        });
 
         // console.log("best cell", bestCell);
 
-        return bestCell;
+        // return bestCell;
 
 
 
@@ -151,126 +232,222 @@ class Sudoku{
     }
 
     /**return all unassigned (empty) cells in the grid */
-    getUnassignedCells(){
+    getUnassignedCells() {
 
-        for(let r=0; r<this.row.length; r++){
-            for(let c=0; c<this.col.length; c++){
-                if(this.board[r][c]===0){
-                    this.empty.push([r, c]);
-                   
+
+        for (let r = 0; r < this.row.length; r++) {
+            for (let c = 0; c < this.col.length; c++) {
+                if (this.board[r][c] === 0) {
+                    if (r in this.empty) {
+                        if(!this.empty[r].includes(c)){
+                            this.empty[r].push(c);
+
+                        }
+
+                        
+
+                    } else {
+                        this.empty[r] = [c];
+                    }
+
                 }
 
 
             }
         }
-       
-        return this.empty;
-        
 
 
     }
 
-    /**Return the number of possible values for the given cell */
-    numPossibleValues(r,c){
-        const options = [1,2,3,4,5,6,7,8,9];
-        const validOptions = [];
-        console.log("row", r, "col", c);
+    /**Return the number of possible values for the given cell 
+     * { 
+     * 1: 
+     *  {
+     *      1:[2, 3], 
+     *      3: [4,9]
+     *  },
+     *  2: {...}...}
+    */
+    numPossibleValues(r, c) {
+       
+        for (let rowKey in this.empty) {
+            this.posValues[rowKey] = {};
 
-        for(let i=0; i<options.length; i++){
-            if(this.isValidAssignment(r, c, options[i])){
-                validOptions.push(options[i]);
-            
-
-            }
+            this.empty[rowKey].forEach(col => {
+                this.posValues[rowKey][col] = [];
+            });
 
         }
 
-        console.log("valid options", validOptions);
+     
 
-        return validOptions;
+        const options = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        for (let rowKey of Object.keys(this.posValues)) {
+            for (let colKey of Object.keys(this.posValues[rowKey])) {
+                for (let i = 0; i < options.length; i++) {
+                    if (this.isValidAssignment(rowKey, colKey, options[i])) {
+                        this.posValues[rowKey][colKey].push(options[i]);
+                    }
+                }
+            }
+        }
+
+
     }
 
-     /** Check if assigning the value to the cell violates Sudoku rules */
-     isValidAssignment(r, c, val){
-        return(
+
+    /** Check if assigning the value to the cell violates Sudoku rules */
+    isValidAssignment(r, c, val) {
+        return (
             !this.isInRow(r, val) &&
             !this.isInCol(r, c, val) &&
             !this.isInSubgrid(r, c, val)
-        
+
         );
 
 
-     }
+    }
 
-     /**returns true or false if value is already in row */
-     isInRow(r, val){
-        // console.log(this.row[r]);
+    /**returns true or false if value is already in row */
+    isInRow(r, val) {
+
         return this.row[r].includes(val);
 
-     }
+    }
 
     /**returns true or false if value is already in col */
-     isInCol(r, c, val){
-        // console.log("board at rc", this.col[c]);
+    isInCol(r, c, val) {
+   
         return this.col[c].includes(val);
 
-     }
+    }
 
-     /**returns true or false if value is already in subgrid */
-     isInSubgrid(r, c, val){
+    /**returns true or false if value is already in subgrid */
+    isInSubgrid(r, c, val) {
         const subgridIndex = this.getSubgridIndex(r, c);
 
         return this.subgrid[subgridIndex].includes(val);
-     }
+    }
 
     /**returns number of constraints involving a cell 
      * iterates over all columns in the given cell's row.
      *  If a cell in the same row is unassigned and not the given cell itself, 
      * it counts as a constraint.
     */
-    countConstraints(r, c){
+    countConstraints(r, c) {
         let constraints = 0;
 
-        this.row[r].forEach((val, index)=>{
-            if(val===0 && index!==c){
+        this.row[r].forEach((val, index) => {
+            if (val === 0 && index !== c) {
                 constraints++;
             }
 
         });
-        this.col[c].forEach((val, index)=>{
-            if(val===0  && index!==r){
+        this.col[c].forEach((val, index) => {
+            if (val === 0 && index !== r) {
                 constraints++;
             }
         });
 
         const subgridIndex = this.getSubgridIndex(r, c);
 
-        this.subgrid[subgridIndex].forEach((val, index)=>{
-            if(val===0){
+        this.subgrid[subgridIndex].forEach((val, index) => {
+            if (val === 0) {
                 constraints++;
             }
 
         });
-        constraints-=1;
+        constraints -= 1;
         console.log("constrainst", constraints);
         return constraints;
 
-        
+
 
     }
 
-     /** Assign the value to the cell */
-     assignValue(r, c, val){
- 
+    /** Assign the value to the cell */
+    assignValue(r, c, val) {
+
+        // console.log("board before", this.board);
+
+      
+
         this.board[r][c] = val;
+        // console.log("board after", this.board);
+
+   
+        this.row[r][c] = val;
+        this.col[c][r] = val;
+   
+
+
+
+     
+
+
+        const subgridIndex = this.getSubgridIndex(r, c);
+        const positionInSubgrid = (r % this.subgridSize) * this.subgridSize + (c % this.subgridSize);
+        // console.log("pos in subgrid", positionInSubgrid);
+        // console.log("news", this.subgrid[subgridIndex]);
+     
+      
+
+        this.subgrid[subgridIndex][positionInSubgrid] = val;
+        // console.log(this.subgrid[subgridIndex]);
+        // console.log("the position", positionInSubgrid);
+        // console.log(this.subgrid[subgridIndex][positionInSubgrid]);
+
+
+
+
+
+        // console.log("the subgrid after", this.subgrid[subgridIndex]);
+
+        console.log("pos vals",this.posValues)
+
+
+
+       
+        delete this.posValues[r][c];
+        console.log("vals after", this.posValues);
+
+
+
+        let start = this.empty[r].indexOf(Number(c));
+        this.empty[r].splice(start,1);
+        console.log("empty", this.empty);
+
+
+        if(Object.keys(this.posValues[r]).length===0){
+            delete this.posValues[r];
+            delete this.empty[r];
+        }
+
+
+        console.log("vals after", this.posValues);
+
+       console.log("empty", this.empty);
+
+  
+
+
+           
+            
+        // }
+       
+        console.log("all board", this.board);
+
+
         
 
-        this.row[c][r] = val;
-        this.col[c][r] = val;
 
-        // console.log("board", this.board);
-        // console.log("rc", this.col[r]);
-        // console.log("row cr", this.row[c][r]);
+        
+
+        
+        // this.empty.splice(foundIndex, 1);
+
+      
 
         // const subgridIndex = this.getSubgridIndex(r, c);
         // this.subgrid[subgridIndex][r,c] = val;
@@ -279,53 +456,56 @@ class Sudoku{
     }
 
 
- 
-    /**Return the row and column indices of the cell */
-    getRowAndColumn(){
+    propagateConstraints() {
 
     }
-    
+
+
+
+
+    /**Return the row and column indices of the cell */
+    getRowAndColumn() {
+
+    }
+
 
     /**Return the starting row and column indices of the 3x3 box that contains the cell */
-    getStartOfBox(){
+    getStartOfBox() {
 
     }
 
-    propagateConstraints(){
 
-    }
-    // Propagate Sudoku constraints across rows, columns, and boxes
-    
-    hasConflict(){
+
+    hasConflict() {
 
     }
     // Check for any conflicts in the grid
 
-    analyzeConflict(){
+    analyzeConflict() {
 
     }
     // Analyze the conflict and learn a new constraint
-    
-    backTrack(){
+
+    backTrack() {
 
     }
     // Backtrack to a previous state, applying the learned constraint
-    
 
-   
-   
-   
-    
+
+
+
+
+
     /**Remove the value from the cell, reverting it to unassigned */
-    unassignValue(){
+    unassignValue() {
 
     }
 
     /** Check if the Sudoku is completely and correctly filled */
-    isSudokuSolved(){
-        for(let r=0; r<this.row.length; r++){
-            for(let c=0; c<this.col.length; c++){
-                if(this.board[r][c]===null){
+    isSudokuSolved() {
+        for (let r = 0; r < this.row.length; r++) {
+            for (let c = 0; c < this.col.length; c++) {
+                if (this.board[r][c] === null) {
                     return false;
                 }
             }
@@ -340,4 +520,4 @@ class Sudoku{
 
 }
 
-module.exports={Sudoku};
+module.exports = { Sudoku };
